1daea51cadcf06e1f1cfb31432f9671a55e18550
abdalla-atalla <60201830+abdalla-atalla@users.noreply.github.com>
2021-08-03 16:31:54 -0700

    edit

--- src/edu/miracosta/cs113/ArrayListStack.java
+++ src/edu/miracosta/cs113/ArrayListStack.java
@@ -1,9 +1,48 @@
 package edu.miracosta.cs113;
 
-public class ArrayListStack {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EmptyStackException;
+import java.util.List;
+
+public class ArrayListStack<E> implements StackInterface<E> {
+
+	// Data fields
+	public List<E> myList;
+
+	// Constructor
 
 	public ArrayListStack() {
-		// TODO Auto-generated constructor stub
+		myList = new ArrayList<E>();
+	}
+
+	@Override
+	public E push(E obj) {
+		myList.add(obj);
+		return obj;
+	}
+
+	@Override
+	public boolean empty() {
+
+		return myList.isEmpty();
+	}
+
+	@Override
+	public E peek() {
+
+		if (empty()) {
+			throw new EmptyStackException();
+		}
+		return myList.get(myList.size() - 1);
+	}
+
+	@Override
+	public E pop() {
+		if (empty()) {
+			throw new EmptyStackException();
+		}
+		return myList.remove(myList.size() - 1);
 	}
 
 }
